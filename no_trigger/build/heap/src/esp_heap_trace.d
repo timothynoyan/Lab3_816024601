e014509a1fa9f8e0299fa83d2eb865ece0d4a1ea
timothynoyan <64538295+timothynoyan@users.noreply.github.com>
2022-11-15 18:02:08 -0400

    Delete no_trigger directory

--- no_trigger/build/heap/src/esp_heap_trace.d
+++
@@ -1,6 +0,0 @@
-src/esp_heap_trace.o: \
- /opt/esp_sdk/ESP8266_RTOS_SDK/components/heap/src/esp_heap_trace.c \
- /home/timothynoyan/esp/gpio/no_trigger/build/include/sdkconfig.h
-
-/home/timothynoyan/esp/gpio/no_trigger/build/include/sdkconfig.h:
-/opt/esp_sdk/ESP8266_RTOS_SDK/components/heap/src/esp_heap_trace.c:
